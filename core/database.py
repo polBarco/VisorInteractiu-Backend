db273cf7f2a5005ef6ed59090a70a3731c884773
polBarco <pol.barco@estudiantat.upc.edu>
2024-11-19 00:00:53 +0100

    connection database

--- core/database.py
+++ core/database.py
@@ -5,20 +5,38 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 load_dotenv()
 
-user = os.getenv("DB_USER")
-password = os.getenv("DB_PASSWORD")
-host = os.getenv("DB_HOST")
-port = os.getenv("DB_PORT")
-database = os.getenv("DB_NAME")
+database_url = os.getenv("DATABASE_URL")
 
-if (user is None) or (password is None) or (host is None) or (port is None) or (database is None):
-    print(f"user: {user}, password: {password}, host: {host}, port: {port}, database: {database}")
+if database_url is None:
+    # Si DATABASE_URL no está definida, usar variables específicas para entorno local
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+    host = os.getenv("DB_HOST")
+    port = os.getenv("DB_PORT")
+    database = os.getenv("DB_NAME")
 
-    raise Exception("Database credentials not found in .env file")
+    if (user is None) or (password is None) or (host is None) or (port is None) or (database is None):
+        print(f"user: {user}, password: {password}, host: {host}, port: {port}, database: {database}")
+        raise Exception("Database credentials not found in .env file")
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://"+user+":"+password+"@"+host+":"+port+"/"+database
+    # Construir la URL de la base de datos con los valores obtenidos de las variables de entorno locales
+    database_url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# user = os.getenv("DB_USER")
+# password = os.getenv("DB_PASSWORD")
+# host = os.getenv("DB_HOST")
+# port = os.getenv("DB_PORT")
+# database = os.getenv("DB_NAME")
+
+# if (user is None) or (password is None) or (host is None) or (port is None) or (database is None):
+#     print(f"user: {user}, password: {password}, host: {host}, port: {port}, database: {database}")
+
+#     raise Exception("Database credentials not found in .env file")
+
+# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://"+user+":"+password+"@"+host+":"+port+"/"+database
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
