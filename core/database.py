8c107e6b3032802c1b6064050df4273031f0b630
polBarco <pol.barco@estudiantat.upc.edu>
2024-11-19 19:19:00 +0100

    database

--- core/database.py
+++ core/database.py
@@ -5,38 +5,20 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 load_dotenv()
 
-database_url = os.getenv("DATABASE_URL")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+database = os.getenv("DB_NAME")
 
-if database_url is None:
-    # Si DATABASE_URL no está definida, usar variables específicas para entorno local
-    user = os.getenv("DB_USER")
-    password = os.getenv("DB_PASSWORD")
-    host = os.getenv("DB_HOST")
-    port = os.getenv("DB_PORT")
-    database = os.getenv("DB_NAME")
+if (user is None) or (password is None) or (host is None) or (port is None) or (database is None):
+    print(f"user: {user}, password: {password}, host: {host}, port: {port}, database: {database}")
 
-    if (user is None) or (password is None) or (host is None) or (port is None) or (database is None):
-        print(f"user: {user}, password: {password}, host: {host}, port: {port}, database: {database}")
-        raise Exception("Database credentials not found in .env file")
+    raise Exception("Database credentials not found in .env file")
 
-    # Construir la URL de la base de datos con los valores obtenidos de las variables de entorno locales
-    database_url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://"+user+":"+password+"@"+host+":"+port+"/"+database
 
-# user = os.getenv("DB_USER")
-# password = os.getenv("DB_PASSWORD")
-# host = os.getenv("DB_HOST")
-# port = os.getenv("DB_PORT")
-# database = os.getenv("DB_NAME")
-
-# if (user is None) or (password is None) or (host is None) or (port is None) or (database is None):
-#     print(f"user: {user}, password: {password}, host: {host}, port: {port}, database: {database}")
-
-#     raise Exception("Database credentials not found in .env file")
-
-# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://"+user+":"+password+"@"+host+":"+port+"/"+database
-
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
-engine = create_engine(database_url)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
