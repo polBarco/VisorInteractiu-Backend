123931f61485c9e75dbb93885504eb07be429053
polBarco <pol.barco@estudiantat.upc.edu>
2025-01-06 22:37:58 +0100

    deploy render

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://polbarco.github.io/VisorInteractiu-Frontend/", "localhost:3000"], 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
